c8ae91d9c01ccf2643c718e1db43790d787b68e9
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-29 11:16:25 -0300

    add setStartTimer

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,88 @@
+function createCards() {
+  gridCards.innerHTML = "";
+  sortedCards.forEach((card) => {
+    gridCards.innerHTML += `
+    <div class="card" name="${card}">
+      <div class="flip front">
+        <img src="../images/${card}.jpg" alt="" />
+      </div>
+      <div class="flip back">
+        <img src="../images/logo-rj.png" alt="" />
+      </div>
+    </div>
+  `;
+  });
+}
+
+function checkGameWin() {
+  const disabledCards = document.querySelectorAll(".disabledCard");
+  if (disabledCards.length === 18) {
+    alert(`Parabéns, você venceu com tempo de ${timer.textContent}`);
+  }
+}
+
+function checkMatchCards() {
+  if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
+    setTimeout(() => {
+      firstCard.children[0].classList.add("disabledCard");
+      secondCard.children[0].classList.add("disabledCard");
+
+      firstCard = "";
+      secondCard = "";
+
+      checkGameWin();
+    }, 1000);
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("flipCard");
+      secondCard.classList.remove("flipCard");
+      firstCard = "";
+      secondCard = "";
+    }, 1000);
+  }
+}
+
+function clickFlipCard() {
+  const arrayCards = document.querySelectorAll(".card");
+  arrayCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      if (card.classList.contains("flipCard")) return;
+
+      console.log(firstCard);
+      console.log(secondCard);
+
+      if (firstCard === "") {
+        card.classList.add("flipCard");
+        firstCard = card;
+      } else if (secondCard === "") {
+        card.classList.add("flipCard");
+        secondCard = card;
+
+        checkMatchCards();
+      }
+    });
+  });
+}
+
+function setStartTimer() {
+  setInterval(() => {
+    const dateNow = new Date();
+    const dateDiff = new Date(dateNow - initialDateTimer);
+
+    const minutes = String(dateDiff.getMinutes()).padStart(2, "0");
+    const seconds = String(dateDiff.getSeconds()).padStart(2, "0");
+
+    timer.innerHTML = `${minutes}:${seconds}`;
+  }, 1000);
+}
+
 const gridCards = document.querySelector(".gridCards");
+const playerName = document.querySelector(".playerName");
+const timer = document.querySelector(".timer");
+
+const storagePlayerName = localStorage.getItem("@memory_game:player_name");
+
+playerName.innerHTML = storagePlayerName;
 
 const cardNames = [
   "card_1",
@@ -26,67 +110,11 @@ const sortedCards = [...arrayCardNames, ...arrayCardNames].sort(
   () => Math.random() - 0.5
 );
 
-gridCards.innerHTML = "";
-sortedCards.forEach((card) => {
-  gridCards.innerHTML += `
-    <div class="card" name="${card}">
-      <div class="flip front">
-        <img src="../images/${card}.jpg" alt="" />
-      </div>
-      <div class="flip back">
-        <img src="../images/logo-rj.png" alt="" />
-      </div>
-    </div>
-  `;
-});
-
-function checkGameWin() {
-  const disabledCards = document.querySelectorAll(".disabledCard");
-  if (disabledCards.length === 18) {
-    alert("Parabéns, você venceu!");
-  }
-}
-
-function checkMatchCards() {
-  if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
-    setTimeout(() => {
-      firstCard.children[0].classList.add("disabledCard");
-      secondCard.children[0].classList.add("disabledCard");
-
-      firstCard = "";
-      secondCard = "";
-
-      checkGameWin();
-    }, 1000);
-  } else {
-    setTimeout(() => {
-      firstCard.classList.remove("flipCard");
-      secondCard.classList.remove("flipCard");
-      firstCard = "";
-      secondCard = "";
-    }, 1000);
-  }
-}
+createCards();
 
 let firstCard = "";
 let secondCard = "";
+clickFlipCard();
 
-const arrayCards = document.querySelectorAll(".card");
-arrayCards.forEach((card) => {
-  card.addEventListener("click", () => {
-    if (card.classList.contains("flipCard")) return;
-
-    console.log(firstCard);
-    console.log(secondCard);
-
-    if (firstCard === "") {
-      card.classList.add("flipCard");
-      firstCard = card;
-    } else if (secondCard === "") {
-      card.classList.add("flipCard");
-      secondCard = card;
-
-      checkMatchCards();
-    }
-  });
-});
+const initialDateTimer = new Date();
+setStartTimer();
