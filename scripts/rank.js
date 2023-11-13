dd9dd02fdf37b16a634a816234400ffc1a902ddf
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-12 21:28:29 -0300

    add storage verification

--- scripts/rank.js
+++ scripts/rank.js
@@ -7,15 +7,16 @@ function backPage() {
 
 const storageRank = JSON.parse(localStorage.getItem("@memory_game:rank"));
 
-const rankSorted = storageRank
-  .sort((a, b) => {
-    if (a.time > b.time) return 1;
-    if (a.time < b.time) return -1;
-    return 0;
-  })
-  .filter((rank, index) => index < 10);
+if (storageRank) {
+  const rankSorted = storageRank
+    .sort((a, b) => {
+      if (a.time > b.time) return 1;
+      if (a.time < b.time) return -1;
+      return 0;
+    })
+    .filter((rank, index) => index < 10);
 
-tableRank.innerHTML = `
+  tableRank.innerHTML = `
   <tr>
     <th>N°</th>
     <th>Nome</th>
@@ -23,14 +24,15 @@ tableRank.innerHTML = `
   </tr>
 `;
 
-for (let i = 0; i < 10; i++) {
-  tableRank.innerHTML += `
+  for (let i = 0; i < 10; i++) {
+    tableRank.innerHTML += `
     <tr>
       <td>${i + 1}</td>
       <td>${rankSorted[i] ? rankSorted[i].name : ""}</td>
       <td>${rankSorted[i] ? rankSorted[i].time : ""}</td>
     </tr>
   `;
+  }
 }
 
 backButton.addEventListener("click", backPage);
