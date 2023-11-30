762d2cdcbcb140015e6de2fe5b2c43bdddf76ce4
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-30 10:59:37 -0300

    add createBodyTableRank

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,41 +1,36 @@
-const backButton = document.querySelector(".backButton");
-const tableRank = document.querySelector(".tableRank");
-
 function backPage() {
   window.history.back();
 }
 
+function createBodyTableRank() {
+  const storageRank = JSON.parse(localStorage.getItem("@memoryGame:rank"));
+
+  let rankSorted;
+  if (storageRank) {
+    rankSorted = storageRank
+      .sort((a, b) => {
+        if (a.time > b.time) return 1;
+        if (a.time < b.time) return -1;
+        return 0;
+      })
+      .filter((rank, index) => index < 10);
+  }
+
+  bodyTableRank.innerHTML = "";
+  for (let i = 0; i < 10; i++) {
+    bodyTableRank.innerHTML += `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${rankSorted ? rankSorted[i]?.name || "" : ""}</td>
+        <td>${rankSorted ? rankSorted[i]?.time || "" : ""}</td>
+      </tr>
+    `;
+  }
+}
+
+const backButton = document.querySelector(".backButton");
+const bodyTableRank = document.querySelector(".tableRank tbody");
+
 backButton.addEventListener("click", backPage);
 
-const storageRank = JSON.parse(localStorage.getItem("@memoryGame:rank"));
-
-let rankSorted;
-if (storageRank) {
-  rankSorted = storageRank
-    .sort((a, b) => {
-      if (a.time > b.time) return 1;
-      if (a.time < b.time) return -1;
-      return 0;
-    })
-    .filter((rank, index) => index < 10);
-}
-
-tableRank.innerHTML = `
-  <thead>
-    <tr>
-      <th>N°</th>
-      <th>Nome</th>
-      <th>Tempo</th>
-    </tr>
-  </thead>
-`;
-
-for (let i = 0; i < 10; i++) {
-  tableRank.innerHTML += `
-    <tr>
-      <td>${i + 1}</td>
-      <td>${rankSorted ? rankSorted[i]?.name || "" : ""}</td>
-      <td>${rankSorted ? rankSorted[i]?.time || "" : ""}</td>
-    </tr>
-  `;
-}
+createBodyTableRank();
